b0e055d3928eed4a9784159b511632888007d421
Resma Adi Nugroho <63656331+dinug-c@users.noreply.github.com>
2022-02-25 23:12:02 +0700

    Add files via upload

---
+++ Pertemuan 1/24060121120021/Tugas1_Resma Adi Nugroho_24060121120021.c
@@ -0,0 +1,12 @@
+/*Nama File :  Tugas1_Resma Adi Nugroho_24060121120021.c*/
+/*Nama Pembuat : Resma Adi Nugroho */
+/*Tanggal : 25 Februari 2022 */
+/*Deskripsi : Membuat program sederhana dengan bahasa C*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    printf("Aku Cinta Alpro \x03!\n");
+    return 0;
+}
