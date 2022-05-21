1dbbed5d696baaaf8b71d2756fa87eb85a2ee792
Wildan Adji <91516723+djine@users.noreply.github.com>
2022-05-21 23:32:25 +0700

    Tugas11_Wildan Adji Prabowo_24060121140097

---
+++ Pertemuan 11/24060121140097/bantuPakEko.c
@@ -0,0 +1,49 @@
+/*Nama File     : Sorcerer Supreme.c*/
+/*Deskripsi     : Membantu Pak Eko rekap nilai */
+/*Pembuat       : 24060121140097 - Wildan Adji Prabowo*/
+/*Tgl Pembuatan : 21 Mei 2022*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Kamus */
+typedef struct{
+    char nama[255][255];
+    int nim, nilai;
+}RMHS;
+
+int main()
+{
+    RMHS Mhs;
+    int retval;
+
+    FILE *fileku;
+    FILE *filekuA, *filekuB, *filekuC,*filekuD;
+    fileku = fopen("rekapNilaiMahasiswa.txt", "r");
+    filekuA = fopen("Lulus_A.txt", "w");
+    filekuB = fopen("Lulus_B.txt", "w");
+    filekuC = fopen("Lulus_C.txt", "w");
+    filekuD = fopen("Lulus_D.txt", "w");
+    retval = fscanf(fileku, "%s %d %d", &Mhs.nama, &Mhs.nim, &Mhs.nilai);
+
+    fprintf(filekuA, "Daftar mahasiswa yang lulus mata kuliah Algoritma Pemrograman dengan predikat A\n\n");
+    fprintf(filekuB, "Daftar mahasiswa yang lulus mata kuliah Algoritma Pemrograman dengan predikat B\n\n");
+    fprintf(filekuC, "Daftar mahasiswa yang lulus mata kuliah Algoritma Pemrograman dengan predikat C\n\n");
+    fprintf(filekuD, "Daftar mahasiswa yang tidak lulus mata kuliah Algoritma Pemrograman dengan predikat D\n\n");
+    while(retval != EOF) {
+        if(Mhs.nilai>=80) {
+            fprintf(filekuA, "%s %d %d\n", Mhs.nama, Mhs.nim, Mhs.nilai);
+        } else if (Mhs.nilai >=70 && Mhs.nilai <80) {
+            fprintf(filekuB, "%s %d %d\n", Mhs.nama, Mhs.nim, Mhs.nilai);
+        } else if (Mhs.nilai >=60 && Mhs.nilai <70) {
+            fprintf(filekuC, "%s %d %d\n", Mhs.nama, Mhs.nim, Mhs.nilai);
+        } else {
+            fprintf(filekuD, "%s %d %d\n", Mhs.nama, Mhs.nim, Mhs.nilai);
+        }
+
+        retval = fscanf(fileku, "%s %d %d", &Mhs.nama, &Mhs.nim, &Mhs.nilai);
+    }
+
+    fclose(fileku);
+}
